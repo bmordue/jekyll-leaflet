e32e14f2e72f78d908db2c2f3bc3928d715f01dd
Ben Mordue <benmordue@gmail.com>
2019-05-15 12:30:50 +0200

    push 0.0.1

--- lib/jekyll-leaflet.rb
+++ lib/jekyll-leaflet.rb
@@ -1,5 +1,4 @@
 require "jekyll"
-
 class LeafletEmbed < Liquid::Tag
 
   def initialize(tagName, content, tokens)
@@ -8,6 +7,23 @@ class LeafletEmbed < Liquid::Tag
   end
 
   def render(context)
+    leaflet_url = "#{context[@content.strip]}"
+    if leaflet_url[/youtu\.be\/([^\?]*)/]
+      @leaflet_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-leaflet-video-id-from-url/4811367#4811367
+      leaflet_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      @leaflet_id = $5
+    end
+
+    tmpl_path = File.join Dir.pwd, "_includes", "leaflet.html"
+    if File.exist?(tmpl_path)
+      tmpl = File.read tmpl_path
+      site = context.registers[:site]
+      tmpl = (Liquid::Template.parse tmpl).render site.site_payload.merge!({"leaflet_id" => @leaflet_id})
+    else
+      %q(<div id=map)
+    end
   end
 
   Liquid::Template.register_tag "leaflet", self

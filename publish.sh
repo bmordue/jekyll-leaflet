7f39c3d69438f7db880565822d29144be4c3bddf
Ben Mordue <benmordue@gmail.com>
2019-05-20 16:29:23 +0200

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
+set -ex
 PREVIOUS_VERSION=0.0.1
 NEXT_VERSION=0.0.2
-sed -i~ 's/$PREVIOUS_VERSION/$NEXT_VERSION/g' jekyll-leaflet.gemspec
-git add jekyll-leaflet.gemspec && git commit -m 'version $NEXT_VERSION'
-docker run --rm -v $(pwd):/wrk -w /wrk ruby:2.6 gem build jekyll-leaflet.gemspec
+GEMSPEC=jekyll-leaflet.gemspec
+sed -i~ "s/$PREVIOUS_VERSION/$NEXT_VERSION/g" $GEMSPEC
+git add $GEMSPEC && git commit -m "version $NEXT_VERSION"
+docker run --rm -v $(pwd):/wrk -w /wrk ruby:2.6 gem build $GEMSPEC
 git tag "v$NEXT_VERSION"
 docker run --rm -v $(pwd):/wrk -w /wrk -v $HOME/.gem/credentials:/.gem/credentials ruby:2.6 gem push jekyll-leaflet-$NEXT_VERSION.gem --config-file /.gem/credentials
+rm $GEMSPEC~
